8a4a04bd96259cf41d49428131bcb5483caf4ac5
Simon Vargas Toro <simon.vt4807@gmail.com>
2026-03-17 20:36:45 +0000

    codigo documentado

--- laboratorio 5/codigo.cpp
+++ laboratorio 5/codigo.cpp
@@ -1,23 +1,31 @@
-#include <DHT.h>		// importa la Librerias DHT
-#include <DHT_U.h>
+#include <DHT.h>
 
-int SENSOR = 2;			// pin DATA de DHT22 a pin digital 2
-int TEMPERATURA;
-int HUMEDAD;
+#define DHTPIN 2      // pin OUT
+#define DHTTYPE DHT11 // tipo de sensor
 
-DHT dht(SENSOR, DHT22);		// creacion del objeto, cambiar segundo parametro
-				// por DHT11 si se utiliza en lugar del DHT22
-void setup(){
-  Serial.begin(9600);		// inicializacion de monitor serial
-  dht.begin();			// inicializacion de sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
 }
 
-void loop(){
-  TEMPERATURA = dht.readTemperature();	// obtencion de valor de temperatura
-  HUMEDAD = dht.readHumidity();		// obtencion de valor de humedad
-  Serial.print("Temperatura: ");	// escritura en monitor serial de los valores
-  Serial.print(TEMPERATURA);
-  Serial.print(" Humedad: ");
-  Serial.println(HUMEDAD);
-  delay(500);
-}
+void loop() {
+
+  float humedad = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+
+  if (isnan(humedad) || isnan(temperatura)) {
+    Serial.println("Error al leer el sensor");
+    return;
+  }
+
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.print(" °C  ");
+
+  Serial.print("Humedad: ");
+  Serial.print(humedad);
+  Serial.println(" %");
+
+  delay(2000); // el DHT11 necesita ~2 segundos entre lecturas
